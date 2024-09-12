9db8463a72d2f844466937ee2fb42cec9b80a4f4
mzbzy10 <tzha578@aucklanduni.ac.nz>
2024-09-12 21:48:50 +1200

    three main methods put in the radio scene controller and the placeholder for the radio.fxml is done

--- src/main/java/nz/ac/auckland/se206/controllers/RadioSceneController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/RadioSceneController.java
@@ -1,3 +1,28 @@
 package nz.ac.auckland.se206.controllers;
 
-public class RadioSceneController {}
+import javafx.event.ActionEvent;
+
+public class RadioSceneController {
+  boolean Playingaudio = false;
+
+  /**
+   * the play button is clicked if the audio is playing, pause it if the audio is paused, play it
+   *
+   * @param event
+   */
+  private void onPlay(ActionEvent event) {}
+
+  /**
+   * the decrease frequency button is clicked change to the left audio
+   *
+   * @param event
+   */
+  private void onDecreaseFrequency(ActionEvent event) {}
+
+  /**
+   * the increase frequency button is clicked change to the right audio
+   *
+   * @param event
+   */
+  private void onIncreaseFrequency(ActionEvent event) {}
+}
