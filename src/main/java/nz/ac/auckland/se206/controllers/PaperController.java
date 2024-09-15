ba367b967b385d30106ccff4d355fb4dbf49fe26
oorjagandhi <oorjagandhi@gmail.com>
2024-09-15 18:21:56 +1200

    applied styling to the paper clue

--- src/main/java/nz/ac/auckland/se206/controllers/PaperController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/PaperController.java
@@ -7,6 +7,7 @@ import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Cursor;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.image.Image;
@@ -28,6 +29,11 @@ public class PaperController {
     "/images/crumple-4.png"
   };
 
+  @FXML
+  public void initialize() {
+    setupPaperImages(); // Ensure this is called to initialize event handlers and images
+  }
+
   // Method to set up the initial image and click handler
   public void setupPaperImages() {
     if (paperImageView != null) {
@@ -35,6 +41,9 @@ public class PaperController {
       paperImageView.setImage(new Image(getClass().getResourceAsStream(paperImages[0])));
       // Set up click event handler
       paperImageView.setOnMouseClicked(event -> handlePaperClick(event));
+
+      paperImageView.setOnMouseEntered(this::handleMouseEnterPaper);
+      paperImageView.setOnMouseExited(this::handleMouseExitPaper);
     }
   }
 
@@ -45,7 +54,28 @@ public class PaperController {
     if (clickCount < paperImages.length) {
       playSound("/sounds/paper.mp3");
       paperImageView.setImage(new Image(getClass().getResourceAsStream(paperImages[clickCount])));
+
+      // If the final image is reached, remove the highlight effect
+      if (clickCount == paperImages.length - 1) {
+        paperImageView.setStyle(""); // Reset style to remove highlight
+        paperImageView.setCursor(Cursor.DEFAULT); // Reset cursor
+      }
     }
+  }
+
+  private void handleMouseEnterPaper(MouseEvent event) {
+    if (clickCount < paperImages.length - 1) {
+      ImageView paper = (ImageView) event.getSource();
+      paper.setCursor(Cursor.HAND); // Change cursor to hand
+      paper.setStyle(
+          "-fx-effect: dropshadow(gaussian, yellow, 10, 0.5, 0, 0);"); // Apply drop shadow effect
+    }
+  }
+
+  private void handleMouseExitPaper(MouseEvent event) {
+    ImageView paper = (ImageView) event.getSource();
+    paper.setCursor(Cursor.DEFAULT);
+    paper.setStyle(""); // Reset to default style
   }
 
   @FXML
